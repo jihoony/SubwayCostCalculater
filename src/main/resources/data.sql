2a35cb2c791d337e197b17ccebf6b4da608d487a
jihoon <jihoon_yang@ksmartech.com>
2021-04-09 11:19:55 +0900

    add more holidays

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,14 @@
+insert into holiday values ('20210101', 'new year');
+insert into holiday values ('20210211', 'lunar new year 1');
+insert into holiday values ('20210212', 'lunar new year 2');
+insert into holiday values ('20210213', 'lunar new year 3');
 insert into holiday values ('20210505', 'children day');
 insert into holiday values ('20210519', 'buddha day');
+insert into holiday values ('20210606', 'memorial day');
+insert into holiday values ('20210815', 'liberation day');
 insert into holiday values ('20210920', 'thanks giving day 1');
 insert into holiday values ('20210921', 'thanks giving day 1');
 insert into holiday values ('20210922', 'thanks giving day 1');
+insert into holiday values ('20211003', 'national foundation day');
+insert into holiday values ('20211009', 'Hangul day');
+insert into holiday values ('20211225', 'christmas');
